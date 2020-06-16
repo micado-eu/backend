4ca5186cc92645f131eb67c317de60ad9c9e616e
Luca Gioppo <gioppoluca@libero.it>
2020-06-16 15:00:27 +0200

    Added changes

--- application/micado-backend/src/migrate.ts
+++ application/micado-backend/src/migrate.ts
@@ -6,7 +6,7 @@ export async function migrate(args: string[]) {
 
   const app = new MicadoBackendApplication();
   await app.boot();
-  await app.migrateSchema({existingSchema});
+  await app.migrateSchema({existingSchema, models: ['FeaturesFlags', 'User']});
 
   // Connectors usually keep a pool of opened connections,
   // this keeps the process running even after all work is done.
