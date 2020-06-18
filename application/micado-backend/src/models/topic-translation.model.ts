240ed30ed6547b6296454b7ecac61e7f13fc3375
Luca Gioppo <gioppoluca@libero.it>
2020-06-18 17:52:46 +0200

    fixed bug with workaround

--- application/micado-backend/src/models/topic-translation.model.ts
+++ application/micado-backend/src/models/topic-translation.model.ts
@@ -42,7 +42,7 @@ export class TopicTranslation extends Entity {
 
   // Indexer property to allow additional data
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [prop: string]: any;
+  //[prop: string]: any;
 
   constructor(data?: Partial<TopicTranslation>) {
     super(data);
