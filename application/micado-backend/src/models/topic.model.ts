e2dcc6150a1990682ef86295e31b83eb347db1d7
giuseppecsi <giuseppe.dorio@csi.it>
2020-07-07 11:12:11 +0200

    Added nullable to all "date" fields

--- application/micado-backend/src/models/topic.model.ts
+++ application/micado-backend/src/models/topic.model.ts
@@ -28,6 +28,7 @@ export class Topic extends Entity {
 
   @property({
     type: 'date',
+    jsonSchema: { nullable: true },
     postgresql: { columnName: 'publication_date', dataType: 'timestamp without time zone', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES' },
   })
   publicationDate?: string;
