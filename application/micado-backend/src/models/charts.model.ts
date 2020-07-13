672a50bce056f9dc08e8f8dcf655e203f737254b
jowie <zyx1993@126.com>
2020-07-13 22:39:34 +0200

    set charts_id to auto-generate

--- application/micado-backend/src/models/charts.model.ts
+++ application/micado-backend/src/models/charts.model.ts
@@ -6,10 +6,11 @@ import {Entity, model, property} from '@loopback/repository';
 export class Charts extends Entity {
   @property({
     type: 'number',
-    required: true,
+    required: false,
     scale: 0,
-    id: 1,
-    postgresql: {columnName: 'id', dataType: 'smallint', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'NO'},
+    id: true,
+    generated: true,
+    // postgresql: {columnName: 'id', dataType: 'smallint', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'YES'},
   })
   id: number;
 
