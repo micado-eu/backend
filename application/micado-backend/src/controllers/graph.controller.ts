d5629ca124923f68bb1ccedce9e18d39002e0c46
Luca Gioppo <gioppoluca@libero.it>
2020-07-14 14:56:36 +0200

    fixed save step and edit step

--- application/micado-backend/src/controllers/graph.controller.ts
+++ application/micado-backend/src/controllers/graph.controller.ts
@@ -76,10 +76,64 @@ export class GraphController {
           });
         }
         await saveTranslations()
+
+        // here we need also to save the documents of the step
       });
     }
     await saveSteps()
     // save translations of new steps
+
+    let changed_steps = data.steps.filter((step: any) => { return (step.is_edited != null && (step.is_edited && !step.is_new)) })
+    console.log("changed_steps")
+    console.log(changed_steps)
+    const editSteps = async () => {
+      await this.asyncForEach(changed_steps, async (cstep: any) => {
+
+        // filter only needed data
+        let editingStep = JSON.parse(JSON.stringify(cstep, ['id', 'cost', 'locationSpecific', 'location', 'locationLon', 'locationLat', 'idProcess']));
+        // save new step
+        await this.stepRepository.updateById(editingStep.id, editingStep)
+          .then(
+            result => {
+              console.log("edited step")
+              console.log(result)
+            }
+          )
+        // save translations
+        const editTranslations = async () => {
+          await this.asyncForEach(cstep.translations, async (transl: any) => {
+
+            let editingTranslation = JSON.parse(JSON.stringify(transl, ['id', 'lang', 'step', 'description']));
+            let trid = cstep.id
+            console.log(editingTranslation)
+            let where = {
+              id: { eq: editingTranslation.id }, lang: { eq: editingTranslation.lang }
+            }
+            this.stepRepository.translations(trid).patch(editingTranslation, where)
+              .then((trres) => {
+                console.log("saved translation")
+                console.log(trres)
+              }).catch(error => {
+                console.log(error)
+              })
+
+          });
+        }
+        await editTranslations()
+
+        // here we need also to save the documents of the step
+      });
+    }
+    await editSteps()
+
+
+    // add the new steplinks
+
+    // edit steplinks
+
+    // delete step links
+
+    // delete step
 
     return result
   }
