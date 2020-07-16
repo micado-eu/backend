39ac09898f193b647ea1ea4b66020f1fdfa97445
Iván Chicano <ivchicano@gmail.com>
2020-07-16 11:53:51 +0200

    Make unpublished event endpoints for cso

--- application/micado-backend/src/controllers/event.controller.ts
+++ application/micado-backend/src/controllers/event.controller.ts
@@ -49,6 +49,32 @@ export class EventController {
     return this.eventRepository.create(event);
   }
 
+  @post('/events/unpublished', {
+    responses: {
+      '200': {
+        description: 'Event model instance (unpublished)',
+        content: { 'application/json': { schema: getModelSchemaRef(Event) } },
+      },
+    },
+  })
+  async createUnpublished(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Event, {
+            title: 'NewUnpublishedEvent',
+            exclude: ['id'],
+            optional: ['published']
+          }),
+        },
+      },
+    })
+    event: Omit<Event, 'id'>,
+  ): Promise<Event> {
+    event.published = false
+    return this.eventRepository.create(event);
+  }
+
   @get('/events/count', {
     responses: {
       '200': {
@@ -164,6 +190,28 @@ export class EventController {
     })
     event: Event,
   ): Promise<void> {
+    await this.eventRepository.updateById(id, event);
+  }
+
+  @patch('/events/{id}/unpublished', {
+    responses: {
+      '204': {
+        description: 'Event PATCH success (marks event as unpublished in the process)',
+      },
+    },
+  })
+  async updateByIdUnpublished(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Event, { partial: true }),
+        },
+      },
+    })
+    event: Event,
+  ): Promise<void> {
+    event.published = false
     await this.eventRepository.updateById(id, event);
   }
 
