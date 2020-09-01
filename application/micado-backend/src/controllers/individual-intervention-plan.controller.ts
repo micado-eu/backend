a888037c3ed145c63e546d94d4e5f563030f4019
Luca Gioppo <gioppoluca@libero.it>
2020-09-01 11:56:55 +0200

    added intervention plan for migrant

--- application/micado-backend/src/controllers/individual-intervention-plan.controller.ts
+++ application/micado-backend/src/controllers/individual-intervention-plan.controller.ts
@@ -16,24 +16,26 @@ import {
   del,
   requestBody,
 } from '@loopback/rest';
-import {IndividualInterventionPlan} from '../models';
-import {IndividualInterventionPlanRepository} from '../repositories';
+import { IndividualInterventionPlan } from '../models';
+import { IndividualInterventionPlanRepository } from '../repositories';
+import { InterventionTypesRepository } from '../repositories';
 
 export class IndividualInterventionPlanController {
   constructor(
     @repository(IndividualInterventionPlanRepository)
-    public individualInterventionPlanRepository : IndividualInterventionPlanRepository,
-  ) {}
+    public individualInterventionPlanRepository: IndividualInterventionPlanRepository,
+    @repository(InterventionTypesRepository) public interventionTypesRepository: InterventionTypesRepository
+  ) { }
 
   @post('/individual-intervention-plans', {
     responses: {
       '200': {
         description: 'IndividualInterventionPlan model instance',
-        content: {'application/json': {schema: getModelSchemaRef(IndividualInterventionPlan)}},
+        content: { 'application/json': { schema: getModelSchemaRef(IndividualInterventionPlan) } },
       },
     },
   })
-  async create(
+  async create (
     @requestBody({
       content: {
         'application/json': {
@@ -53,11 +55,11 @@ export class IndividualInterventionPlanController {
     responses: {
       '200': {
         description: 'IndividualInterventionPlan model count',
-        content: {'application/json': {schema: CountSchema}},
+        content: { 'application/json': { schema: CountSchema } },
       },
     },
   })
-  async count(
+  async count (
     @param.where(IndividualInterventionPlan) where?: Where<IndividualInterventionPlan>,
   ): Promise<Count> {
     return this.individualInterventionPlanRepository.count(where);
@@ -71,14 +73,14 @@ export class IndividualInterventionPlanController {
           'application/json': {
             schema: {
               type: 'array',
-              items: getModelSchemaRef(IndividualInterventionPlan, {includeRelations: true}),
+              items: getModelSchemaRef(IndividualInterventionPlan, { includeRelations: true }),
             },
           },
         },
       },
     },
   })
-  async find(
+  async find (
     @param.filter(IndividualInterventionPlan) filter?: Filter<IndividualInterventionPlan>,
   ): Promise<IndividualInterventionPlan[]> {
     return this.individualInterventionPlanRepository.find(filter);
@@ -88,15 +90,15 @@ export class IndividualInterventionPlanController {
     responses: {
       '200': {
         description: 'IndividualInterventionPlan PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
+        content: { 'application/json': { schema: CountSchema } },
       },
     },
   })
-  async updateAll(
+  async updateAll (
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(IndividualInterventionPlan, {partial: true}),
+          schema: getModelSchemaRef(IndividualInterventionPlan, { partial: true }),
         },
       },
     })
@@ -112,15 +114,15 @@ export class IndividualInterventionPlanController {
         description: 'IndividualInterventionPlan model instance',
         content: {
           'application/json': {
-            schema: getModelSchemaRef(IndividualInterventionPlan, {includeRelations: true}),
+            schema: getModelSchemaRef(IndividualInterventionPlan, { includeRelations: true }),
           },
         },
       },
     },
   })
-  async findById(
+  async findById (
     @param.path.number('id') id: number,
-    @param.filter(IndividualInterventionPlan, {exclude: 'where'}) filter?: FilterExcludingWhere<IndividualInterventionPlan>
+    @param.filter(IndividualInterventionPlan, { exclude: 'where' }) filter?: FilterExcludingWhere<IndividualInterventionPlan>
   ): Promise<IndividualInterventionPlan> {
     return this.individualInterventionPlanRepository.findById(id, filter);
   }
@@ -132,12 +134,12 @@ export class IndividualInterventionPlanController {
       },
     },
   })
-  async updateById(
+  async updateById (
     @param.path.number('id') id: number,
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(IndividualInterventionPlan, {partial: true}),
+          schema: getModelSchemaRef(IndividualInterventionPlan, { partial: true }),
         },
       },
     })
@@ -153,7 +155,7 @@ export class IndividualInterventionPlanController {
       },
     },
   })
-  async replaceById(
+  async replaceById (
     @param.path.number('id') id: number,
     @requestBody() individualInterventionPlan: IndividualInterventionPlan,
   ): Promise<void> {
@@ -167,7 +169,30 @@ export class IndividualInterventionPlanController {
       },
     },
   })
-  async deleteById(@param.path.number('id') id: number): Promise<void> {
+  async deleteById (@param.path.number('id') id: number): Promise<void> {
     await this.individualInterventionPlanRepository.deleteById(id);
+  }
+
+
+  @get('/individual-intervention-plans-migrant', {
+    responses: {
+      '200': {
+        description: 'Array of IndividualInterventionPlan model instances',
+      },
+    },
+  })
+  async find_migrant (
+    @param.query.string('lang') lang = 'en',
+    @param.query.number('user_id') user_id: number
+  ): Promise<void> {
+
+    //    return this.individualInterventionPlanRepository.dataSource.execute("select json_agg(jj.*)::jsonb from (select iip.*, (select json_agg(kk.*)::jsonb from (SELECT *,(select itt.intervention_title from intervention_types_translation itt where itt.id=iipi.intervention_type and lang='" + lang + "'), (select ip.process_id from intervention_processes ip where ip.intervention_type = iipi.intervention_type), (select json_agg(itv.tenant_id)::jsonb from intervention_type_validator itv where itv.intervention_type_id=iipi.intervention_type) as validators  FROM individual_intervention_plan_interventions iipi where iipi.list_id = iip.id) kk) as interventions from individual_intervention_plan iip where iip.user_id=" + user_id + ") jj");
+    return this.individualInterventionPlanRepository.dataSource.execute("select iip.*, (select json_agg(kk.*)::jsonb from (SELECT *,(select itt.intervention_title from intervention_types_translation itt where itt.id=iipi.intervention_type and lang='" + lang + "'), (select ip.process_id from intervention_processes ip where ip.intervention_type = iipi.intervention_type), (select json_agg(itv.tenant_id)::jsonb from intervention_type_validator itv where itv.intervention_type_id=iipi.intervention_type) as validators  FROM individual_intervention_plan_interventions iipi where iipi.list_id = iip.id) kk) as interventions from individual_intervention_plan iip where iip.user_id=" + user_id);
+  }
+
+  async asyncForEach (array: any, callback: any) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
   }
 }
