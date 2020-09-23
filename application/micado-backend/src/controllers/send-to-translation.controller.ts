f754b0bbfb6e3010c637063d149ce6f0bed5e201
MathiasB <git@example.com>
2020-09-23 11:05:16 +0200

    translations to file

--- application/micado-backend/src/controllers/send-to-translation.controller.ts
+++ application/micado-backend/src/controllers/send-to-translation.controller.ts
@@ -24,6 +24,25 @@ import { TopicTranslationRepository } from '../repositories';
 import fs from 'fs';
 
 
+
+var config = {
+  MICADO_BACKEND_URL: process.env.MICADO_BACKEND_URL,
+  MICADO_GIT_URL: process.env.MICADO_GIT_URL,
+  MICADO_GIT_PASSWORD: process.env.MICADO_GIT_PASSWORD,
+  TRANSLATIONS_DIR: "/tmp/translations"
+}
+
+if (!config.MICADO_BACKEND_URL) {
+  console.log('MICADO_BACKEND_URL must be set');
+  process.exit(1)
+}
+if (!config.MICADO_GIT_URL) {
+  console.log('MICADO_GIT_URL must be set');
+  process.exit(1)
+}
+
+
+
 export class SendToTranslationController {
   constructor(
     @repository(TopicTranslationRepository) public topicTranslationRepository: TopicTranslationRepository,
@@ -50,8 +69,15 @@ export class SendToTranslationController {
 
 
     let lang = 'en'
-    let query = 'select array_to_json(array_agg(k)) from(select json_build_object(id, (select row_to_json(t) from(select gl.title, gl.description from glossary_translation as gl where gl.id = glossary_translation.id and gl.lang = \'$1\') as t)) as "record" from glossary_translation where lang = \'$1\' and "translationState" = 1) as k'
-    this.generateFile('glossary', query, lang)
+    let model = 'glossery'
+    
+    let records = this.getTranslationForModelAndLang(model, lang)
+    
+    let filename = this.buildFileName(model, lang)
+    
+    this.jsonToFile(records, filename)
+    
+    
 
     // for each language we select from all _translation tables the rows that have translationState = 2 and get the data
     // write the data to file in the repository copy in the local FS
@@ -90,13 +116,56 @@ export class SendToTranslationController {
     return "gioppo";
   }
 
-  generateFile (filename: String, query: String, lang: String) {
-    this.topicTranslationRepository.dataSource.execute(query, [lang])
-      .then(result => {
-        console.log(result[0].array_to_json)
-        fs.writeFile(filename + "." + lang + ".json", JSON.stringify(result[0].array_to_json), (err) => {
-          if (err) console.log(err)
-        })
-      })
+  
+  
+  /**
+   * Get all the translations for the given model
+   *
+   * @param model 
+   * @param lang 
+   */
+  getTranslationForModelAndLang(model: String, lang: String) {
+    let query = 'select array_to_json(array_agg(k)) from(select json_build_object(id, (select row_to_json(t) from(select gl.title, gl.description from glossary_translation as gl where gl.id = glossary_translation.id and gl.lang = \'$1\') as t)) as "record" from glossary_translation where lang = \'$1\' and "translationState" = 1) as k'
+    
+    let recordsForLang = {}
+    
+    await this.topicTranslationRepository.dataSource.execute(query, [lang]).then(result => {
+      recordsForLang = result[0].array_to_json
+    }) 
+    
+    return recordsForLang
+  }
+  
+  
+  /**
+   * Build the filename for the translation file based on the language and the model.
+   * 
+   * @param model 
+   * @param lang 
+   */
+  buildFileName(model: String, lang: String) {
+    return `${model}.${lang}.json`
+  }
+  
+  
+  /**
+   * Save the translation object to json file. 
+   * 
+   * @param data 
+   * @param filename
+   */
+  jsonToFile(data: any, filename: String) {
+    data = JSON.stringify(data);
+    fs.writeFileSync(`${config.TRANSLATIONS_DIR}/${filename}`, data);
+  }
+  
+  /**
+   * Read the translation object from a json file. 
+   * 
+   * @param filename
+   */
+  fileToJSon(filename: String) {
+    let raw_data = fs.readFileSync(`${config.TRANSLATIONS_DIR}/${filename}`);
+    return JSON.parse(raw_data)
   }
 }
