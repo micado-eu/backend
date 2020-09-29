cf27a08502553c91f6472b9459f48643ed34a17a
Luca Gioppo <gioppoluca@libero.it>
2020-09-29 09:12:21 +0200

    fixing folder

--- application/micado-backend/src/controllers/send-to-translation.controller.ts
+++ application/micado-backend/src/controllers/send-to-translation.controller.ts
@@ -30,7 +30,7 @@ var config = {
   MICADO_BACKEND_URL: process.env.MICADO_BACKEND_URL,
   MICADO_GIT_URL: process.env.MICADO_GIT_URL,
   MICADO_GIT_PASSWORD: process.env.MICADO_GIT_PASSWORD,
-  TRANSLATIONS_DIR: "/tmp/translations"
+  TRANSLATIONS_DIR: "/tmp"
 }
 
 if (!config.MICADO_GIT_URL) {
@@ -63,20 +63,20 @@ export class SendToTranslationController {
     // Initial git setup
     await git
       .init()
-      .addRemote('origin', config.MICADO_GIT_URL) 
-      .commit('Initial Commit', {'--allow-empty': null})
+      .addRemote('origin', config.MICADO_GIT_URL)
+      .commit('Initial Commit', { '--allow-empty': null })
       .push(['-u', 'origin', 'master'])
       .catch(() => {
         // We silently fail when the remote is already set.
         // We cant to run all of the above commands only the first time to make sure all is setup well.
       })
-  
-  
+
+
     // Sync from git remote
     console.log("Pulling remote changes...")
-  
+
     await git
-      .pull('origin', 'master', {'--no-edit': null})
+      .pull('origin', 'master', { '--no-edit': null })
       .catch((err: Error) => {
         console.log(JSON.stringify(err, ["message", "arguments", "type", "name"]))
         process.exit(1)
@@ -91,14 +91,14 @@ export class SendToTranslationController {
 
     let lang = 'en'
     let model = 'glossery'
-    
+
     let records = await this.getTranslationForModelAndLang(model, lang)
-    
+
     let filename = this.buildFileName(model, lang)
-    
+
     this.jsonToFile(records, filename)
-    
-    
+
+
 
     // for each language we select from all _translation tables the rows that have translationState = 2 and get the data
     // write the data to file in the repository copy in the local FS
@@ -107,14 +107,14 @@ export class SendToTranslationController {
 
     // after the cycle and after having saved all the files (and overwritten the previous files) add all to a commit and push to GITEA
     console.log(` > Committing and pushing language file`)
-    
-      git
-        .add(filename)
-        .commit(`Updated translation file: ${filename}`)
-        .push()
-        .catch((err: Error) => {
-          console.log(JSON.stringify(err, ["message", "arguments", "type", "name"]))
-        });
+
+    git
+      .add(filename)
+      .commit(`Updated translation file: ${filename}`)
+      .push()
+      .catch((err: Error) => {
+        console.log(JSON.stringify(err, ["message", "arguments", "type", "name"]))
+      });
 
 
 
@@ -145,55 +145,55 @@ export class SendToTranslationController {
     return "koekestad";
   }
 
-  
-  
+
+
   /**
    * Get all the translations for the given model
    *
    * @param model 
    * @param lang 
    */
-  async getTranslationForModelAndLang(model: String, lang: String) {
+  async getTranslationForModelAndLang (model: String, lang: String) {
     let query = 'select array_to_json(array_agg(k)) from(select json_build_object(id, (select row_to_json(t) from(select gl.title, gl.description from glossary_translation as gl where gl.id = glossary_translation.id and gl.lang = \'$1\') as t)) as "record" from glossary_translation where lang = \'$1\' and "translationState" = 1) as k'
-    
+
     let recordsForLang = {}
-    
+
     await this.topicTranslationRepository.dataSource.execute(query, [lang]).then(result => {
       recordsForLang = result[0].array_to_json
-    }) 
-    
+    })
+
     return recordsForLang
   }
-  
-  
+
+
   /**
    * Build the filename for the translation file based on the language and the model.
    * 
    * @param model 
    * @param lang 
    */
-  buildFileName(model: String, lang: String) {
+  buildFileName (model: String, lang: String) {
     return `${model}.${lang}.json`
   }
-  
-  
+
+
   /**
    * Save the translation object to json file. 
    * 
    * @param data 
    * @param filename
    */
-  jsonToFile(data: any, filename: String) {
+  jsonToFile (data: any, filename: String) {
     data = JSON.stringify(data);
     fs.writeFileSync(`${config.TRANSLATIONS_DIR}/${filename}`, data);
   }
-  
+
   /**
    * Read the translation object from a json file. 
    * 
    * @param filename
    */
-  fileToJSon(filename: String) {
+  fileToJSon (filename: String) {
     let raw_data = fs.readFileSync(`${config.TRANSLATIONS_DIR}/${filename}`);
     return JSON.parse(raw_data.toString())
   }
