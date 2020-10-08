fa881b85a851e6eeab9b3554e445d388fd4952d9
giuseppecsi <giuseppe.dorio@csi.it>
2020-10-08 12:00:45 +0200

    fixing datasource

--- application/micado-backend/src/datasources/index.ts
+++ application/micado-backend/src/datasources/index.ts
@@ -3,4 +3,4 @@ export * from './identityserver.datasource';
 export * from './weblate.datasource';
 export * from './countly.datasource';
 export * from './identity-tenant-ds.datasource';
-export * from './pippo.datasource';
+//export * from './pippo.datasource';
