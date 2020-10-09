9260f4732e2d35bafef61e71bab8e5ba4fbc683b
Luca Gioppo <gioppoluca@libero.it>
2020-10-09 15:51:25 +0200

    fix merge conflicts

--- application/micado-backend/src/controllers/identity-tenant-manager.controller.ts
+++ application/micado-backend/src/controllers/identity-tenant-manager.controller.ts
@@ -1,5 +1,5 @@
 // Uncomment these imports to begin using these cool features!
-import { get, param, HttpErrors } from '@loopback/rest';
+import { get, post, param, HttpErrors } from '@loopback/rest';
 
 // import {inject} from '@loopback/context';
 var soap = require('soap');
@@ -32,7 +32,7 @@ export class IdentityTenantManagerController {
 
 
   @post('/addTenant1/{tenant}')
-  async getTenant (
+  async addTenant1 (
     @param.path.string('tenant') tenantDomain: String,
     @param.path.string('tenant') password: String,
   ): Promise<any> {
--- application/micado-backend/src/datasources/index.ts
+++ application/micado-backend/src/datasources/index.ts
@@ -2,7 +2,3 @@ export * from './micado-ds.datasource';
 export * from './identityserver.datasource';
 export * from './weblate.datasource';
 export * from './countly.datasource';
-//export * from './identity-tenant-ds.datasource';
-//export * from './pippo.datasource';
-export * from './tenant-mgt-admin-service.datasource';
-export * from './test.datasource';
