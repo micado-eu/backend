4f253afa8b7dac9b998588f1e88c4c851c45ba8d
D'ORIO Giuseppe 2098 <giuseppe.dorio@csi.it>
2022-05-26 13:17:09 +0200

    added api for e-transl

--- application/micado-backend/src/controllers/weblateproxy.controller.ts
+++ application/micado-backend/src/controllers/weblateproxy.controller.ts
@@ -1,15 +1,17 @@
 // Uncomment these imports to begin using these cool features!
 
-import { inject } from '@loopback/context';
-import { get, param, HttpErrors } from '@loopback/rest';
+import {inject} from '@loopback/context';
+import {get, post, requestBody, RequestContext} from '@loopback/rest';
 import {
   WeblateService
-} from '../services/weblate.service'
+} from '../services/weblate.service';
 
 export class WeblateproxyController {
   constructor(
     @inject('services.Weblate')
     protected weblateService: WeblateService,
+    @inject.context()
+    public context: RequestContext,
   ) { }
 
   @get('/translations')
@@ -22,11 +24,27 @@ export class WeblateproxyController {
   }
 
   calcAuth () {
-    var b = Buffer.from(process.env.WEBLATE_ADMIN_NAME + ':' + process.env.WEBLATE_ADMIN_PASSWORD);
+    const b = Buffer.from(process.env.WEBLATE_ADMIN_NAME + ':' + process.env.WEBLATE_ADMIN_PASSWORD);
     // If we don't use toString(), JavaScript assumes we want to convert the object to utf8.
     // We can make it convert to other formats by passing the encoding type to toString().
-    var s = b.toString('base64');
+    const s = b.toString('base64');
     return s
+  }
+
+
+
+  @post('/e-translations')
+  async webhook(
+    @requestBody({
+      content: {
+        'application/x-www-form-urlencoded': {
+          schema: { type: 'object' },
+        }
+      }
+    }) translation: Object
+  ): Promise<any> {
+    console.log(translation)
+
   }
 
 
