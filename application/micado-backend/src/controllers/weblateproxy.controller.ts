556a993d562d73d3f406b127fb5ea799f35683c9
Luca Gioppo <gioppoluca@libero.it>
2022-06-24 17:42:13 +0200

    added json for managing sql code for updating after etranslation

--- application/micado-backend/src/controllers/weblateproxy.controller.ts
+++ application/micado-backend/src/controllers/weblateproxy.controller.ts
@@ -1,6 +1,6 @@
 // Uncomment these imports to begin using these cool features!
 
-import {inject} from '@loopback/context';
+import { inject, JSONObject } from '@loopback/context';
 import {
   Count,
   CountSchema,
@@ -9,8 +9,8 @@ import {
   repository,
   Where,
 } from '@loopback/repository';
-import {get, post,param, requestBody, RequestContext} from '@loopback/rest';
-import { EventRepository} from '../repositories';
+import { get, post, param, requestBody, RequestContext } from '@loopback/rest';
+import { EventRepository } from '../repositories';
 import {
   WeblateService
 } from '../services/weblate.service';
@@ -26,7 +26,7 @@ export class WeblateproxyController {
   ) { }
 
   @get('/translations')
-  async translations (
+  async translations(
   ): Promise<any> {
     //Preconditions
 
@@ -34,7 +34,7 @@ export class WeblateproxyController {
 
   }
 
-  calcAuth () {
+  calcAuth() {
     const b = Buffer.from(process.env.WEBLATE_ADMIN_NAME + ':' + process.env.WEBLATE_ADMIN_PASSWORD);
     // If we don't use toString(), JavaScript assumes we want to convert the object to utf8.
     // We can make it convert to other formats by passing the encoding type to toString().
@@ -57,21 +57,32 @@ export class WeblateproxyController {
     @param.query.string('target-language') target_language: string,
     @param.query.string('external-reference') external_reference: string,
   ): Promise<any> {
+//$1 is id, $2 is test $3 is lang
+    let sqlarray:JSONObject = {
+      "topic": { topic: "insert into micadoapp.topic_translation (id,topic,lang,translation_date,\"translationState\",published) VALUES ($1 ,$2,$3,now(),1,false) on conflict (id,lang) do update set topic=excluded.topic,translation_date=excluded.translation_date,\"translationState\"=excluded.\"translationState\"" },
+      "process": {
+        process: "insert into micadoapp.process_translation (id,process,description,lang,translation_date,\"translationState\",published) VALUES ($1 ,$2,'',$3,now(),1,false) on conflict (id,lang) do update set process=excluded.process,translation_date=excluded.translation_date,\"translationState\"=excluded.\"translationState\"",
+        description: "insert into micadoapp.process_translation (id,process,description,lang,translation_date,\"translationState\",published) VALUES ($1 ,'',$2,$3,now(),1,false) on conflict (id,lang) do update set description=excluded.description,translation_date=excluded.translation_date,\"translationState\"=excluded.\"translationState\""
+      },
+
+    }
+
     const rawBody = translation.toString();
     console.log(rawBody)
     let lang = target_language.toLowerCase()
     let references = external_reference.split(':')
     console.log(lang)
     console.log(references)
-let buff = new Buffer(rawBody, 'base64');
-let text = buff.toString('utf8');
-
+    let buff = new Buffer(rawBody, 'base64');
+    let text = buff.toString('utf8');
     console.log(text)
-    let sql = "update " + references[0]+"_translation SET " + references[1] + "=$1 where lang=$2 and id=$3"
-    let result = await this.eventRepository.dataSource.execute(sql , [text, lang, references[2]]);
+    let tablejson:any = sqlarray[references[0]]
+    let sql2 =tablejson[references[1]]
+    console.log(sql2)
+    let result = await this.eventRepository.dataSource.execute(sql2, [references[2], text, lang ]);
     console.log(result)
 
-    console.log(sql)
+    console.log(sql2)
   }
 
 
